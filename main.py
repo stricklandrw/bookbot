77688112f940398cb602e16c1b797155615550fb
stricklandrw <stricklandrw@gmail.com>
2025-12-17 12:17:07 -0500

    CH3,L1 - Print a report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from stats import how_many_words, character_count
+from stats import how_many_words, character_count, pretty_report
 
 book_loc = "books/frankenstein.txt"
 
@@ -11,8 +11,15 @@ def get_book_text(path_to_file):
     return
 
 def main():
-    how_many_words(book_loc)
-    character_count(book_loc)
+    wordcount = how_many_words(book_loc)
+    charcount = character_count(book_loc)
+    charcount_report = pretty_report(book_loc)
+    print(f"============ BOOKBOT ============\r\nAnalyzing book found at {book_loc}...\r\n----------- Word Count ----------")
+    print(f"Found {wordcount} total words")
+    print("--------- Character Count -------")
+    for char in charcount_report:
+        print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
     return
 
 main()
--- stats.py
+++ stats.py
@@ -4,8 +4,7 @@ def how_many_words(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         num_words = len(file_contents.split())
-        print(f"Found {num_words} total words")
-    return
+    return num_words
 
 def character_count(path_to_file):
     letter_count = {}
@@ -15,5 +14,17 @@ def character_count(path_to_file):
         for char in file_lowercase:
             if char not in letter_count:
                 letter_count.update({char: file_lowercase.count(char)})
-        print(letter_count)
-    return
+    return letter_count
+
+def pretty_report(path_to_file):
+    char_dict = character_count(path_to_file)
+    chars_list = []
+    sorted_list = []
+    for char in char_dict:
+        if not char.isalpha():
+            continue
+        chars_list.append({"char": char, "num": char_dict[char]})
+#    print(chars_list)
+    sorted_list = sorted(chars_list, reverse=True, key=lambda x: x["num"])
+#    print(sorted_list)
+    return sorted_list
